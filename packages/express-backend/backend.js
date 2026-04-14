4997914dde014b8526d67cd8bca9b3faa0bf08d2
Sushanth Rao <sushanthrao11@gmail.com>
2026-04-14 12:42:24 -0700

    Add express backend with GET, POST, DELETE routes

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,85 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const users = {
+  users_list: [
+    { id: "xyz789", name: "Charlie", job: "Janitor" },
+    { id: "abc123", name: "Mac", job: "Bouncer" },
+    { id: "ppp222", name: "Mac", job: "Professor" },
+    { id: "yat999", name: "Dee", job: "Aspiring actress" },
+    { id: "zap555", name: "Dennis", job: "Bartender" },
+  ],
+};
+
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter((user) => {
+    return (
+      (name === undefined || user["name"] === name) &&
+      (job === undefined || user["job"] === job)
+    );
+  });
+};
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+const deleteUserById = (id) => {
+  users["users_list"] = users["users_list"].filter(
+    (user) => user["id"] !== id
+  );
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+  if (name !== undefined || job !== undefined) {
+    let result = findUserByNameAndJob(name, job);
+    res.send({ users_list: result });
+  } else {
+    res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.status(200).send(userToAdd);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    deleteUserById(id);
+    res.status(204).send();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
